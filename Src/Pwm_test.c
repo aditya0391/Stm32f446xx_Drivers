6f6b1d55127ae87a8670b5f2a76f42d7a50ae678
Aditya Vilayatkar <adityavilayatkar123@gmail.com>
2026-04-21 00:38:54 +0530

    PWM using only Timer and LED - Not working

--- Src/Pwm_test.c
+++ Src/Pwm_test.c
@@ -39,5 +39,28 @@ int main(void)
 
 	GPIO_Init(&Gpio_timer);
 
+	TIM9->PSC = 14999U;
+	TIM9->CR1 |= 1 << 0;         /* Enable the counter*/
+	           /* Di1vide 90 MHz by 9000 → 10 kHz tick */
+	//TIM9->ARR = 999;             /* Counter will reset every 1000 ticks*/
+
+	while(1)
+	{
+
+	    if (TIM9->CNT <= 109U)
+	    {
+		    GPIO_WriteToOutputPin(GPIOA, 5,1);
+	    }
+	    else
+	    {
+		    GPIO_WriteToOutputPin(GPIOA, 5,0);
+	    }
+
+	    if(TIM9->CNT >= 6000U)
+	    {
+		    TIM9->CNT = 0U;
+	    }
+
+	}
     return 0;
 }
